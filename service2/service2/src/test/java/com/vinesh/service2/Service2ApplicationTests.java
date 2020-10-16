adae10d1d9d8081bb6da2e165f4f091a26c5489c
Sai Vinesh <vineshkumar.1996@gmail.com>
2020-10-16 17:47:15 +0530

    initial commit

---
+++ service2/service2/src/test/java/com/vinesh/service2/Service2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinesh.service2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
